7cd63c58ab6599f04c4af06cfbf9eff28c5ffac2
Ankui <youshihou5@gmail.com>
2022-07-09 16:02:36 +0800

    update code

---
+++ sources/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ankui on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
